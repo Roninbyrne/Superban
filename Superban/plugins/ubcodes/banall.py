d01e658368586211d9d0af236e5cdc4b0e9e806d
Ronin <bhabyaprakash01@gmail.com>
2025-07-08 12:07:51 +0530

    Update banall.py

--- Superban/plugins/ubcodes/banall.py
+++ Superban/plugins/ubcodes/banall.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 from Superban import app
 import asyncio
-from config import RECORD_CHAT_ID
+from config import STORAGE_CHANNEL_ID
 
 UTC = ZoneInfo('UTC')
 
@@ -60,7 +60,7 @@ async def ban_all(client, message):
         f"Time in UTC: {utc_time}"
     )
 
-    await app.send_message(RECORD_CHAT_ID, action_details)
+    await app.send_message(STORAGE_CHANNEL_ID, action_details)
     await response_message.edit(f"Banned {banned_count} members" if banned_count > 0 else "No members to ban.")
     await asyncio.sleep(5)
     await response_message.delete()
